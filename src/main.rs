a2e7b88a0ea7bad6a26dc0b55ae4ca0d0e26d909
Lyndon Sanche <lsanche@lyndeno.ca>
2022-01-15 17:27:55 -0700

    Trying to do things

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ Need assistance?
 const HELP_COMMAND: &str = "!help";
 
 const ADD_COMMAND: &str = "!add";
+const MATH_COMMAND: &str = "!math";
 
 struct Handler;
 
@@ -31,19 +32,25 @@ impl EventHandler for Handler {
             send_message(ctx, msg, HELP_MESSAGE).await;
         }
         else if msg.content.contains(ADD_COMMAND) {
-            let mut mysplit = msg.content.split_whitespace();
-            mysplit.next();
+            let add_args = msg.content.replace(ADD_COMMAND, "");
+            let mysplit = add_args.split_whitespace();
             let mut sum: f64 = 0.0;
             for number in mysplit {
                 sum = sum + number.parse::<f64>().unwrap();
             }
-            //if let Err(why) = msg.channel_id.say(&ctx.http, "Result is: ".to_string() + &sum.to_string()).await {
-            //println!("Error sending message: {:?}", why);
-            //}
             send_message(ctx, msg, &("Result is: ".to_string() + &sum.to_string())).await;
         }
+        else if msg.content.contains(MATH_COMMAND) {
+            let spaced_equation = msg.content.replace(MATH_COMMAND, " ");
+            let equation = remove_whitespace(spaced_equation);
+            let mut prompt = String::new();
+            for substring in equation.split(char::is_bracket) {
+                prompt.push_str(substring);
+                prompt.push_str("\n");
+            }
+            send_message(ctx, msg, &prompt).await;
+        }
     }
-
 
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
@@ -53,6 +60,25 @@ impl EventHandler for Handler {
 async fn send_message(ctx: Context, msg: Message, message: &str) {
     if let Err(why) = msg.channel_id.say(&ctx.http, message).await {
         println!("Error sending message: {:?}", why);
+    }
+}
+
+fn remove_whitespace(mut s: String) -> String {
+    s.retain(|c| !c.is_whitespace());
+    s
+}
+
+trait Brackets {
+    fn is_bracket(self) -> bool;
+}
+
+impl Brackets for char {
+    fn is_bracket(self) -> bool {
+        match self {
+            '(' => true,
+            ')' => true,
+            _ => false,
+        }
     }
 }
 
